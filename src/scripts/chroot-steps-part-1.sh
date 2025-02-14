20051f6fe4248983d6db17a2c3109a910bb77fef
engstk <eng.stk@sapo.pt>
2025-02-14 11:52:31 +0000

    Use hwe kernel in noble
    Signed-off-by: engstk <eng.stk@sapo.pt>

--- src/scripts/chroot-steps-part-1.sh
+++ src/scripts/chroot-steps-part-1.sh
@@ -162,7 +162,7 @@ pkgs_specific_to_ubuntu2410_oracular=(
 )
 
 pkgs_specific_to_ubuntu2404_noble=(
-                       "linux-generic"
+                       "linux-generic-hwe-24.04"
                        "xserver-xorg"
                        "xserver-xorg-video-all"
                        "xserver-xorg-video-intel"
